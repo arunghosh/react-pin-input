23828d4ea9eadc409882d882da6931ae7a006f97
Arun Ghosh <arunghosh@gmail.com>
2017-03-26 11:13:59 +0530

    corrected test

--- test/PinInput.spec.jsx
+++ test/PinInput.spec.jsx
@@ -4,6 +4,6 @@ import PinInput from '../src/PinInput';
 import PinItem from '../src/PinItem';
 
 test('The inputs should be equal to the length specified', () => {
-  const pinInput = shallow(<PinInput length={4} />);
+  const pinInput = shallow(<PinInput length={5} />);
   expect(pinInput.find(PinItem)).toHaveLength(5);
 });
