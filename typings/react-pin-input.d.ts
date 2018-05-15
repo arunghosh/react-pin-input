e1764f9fb9b862b2ba0b63f2131513a7e2586cdc
Ronald van der Kooij <ronald@sharpsolutions.nl>
2018-05-15 13:24:17 +0200

    added type definition file

---
+++ typings/react-pin-input.d.ts
@@ -0,0 +1,25 @@
+declare module 'react-pin-input' {
+    import * as React from 'react';
+
+    type InputType = 'numeric' | 'custom';
+
+    interface PinInputProps {
+        length: number;
+        type?: InputType;
+        secret?: boolean;
+        focus?: boolean;
+        onChange?: (value: string, index: number) => void;
+        onComplete?: (value: string, index: number) => void;
+        style?: React.CSSProperties;
+        inputStyle?: React.CSSProperties;
+        inputFocusStyle?: React.CSSProperties;
+        validate?: (value: string) => string;
+    }
+
+    class PinInput extends React.Component<PinInputProps> {
+        clear: () => void;
+        focus: () => void;
+    }
+
+    export default PinInput;
+}
